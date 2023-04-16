b1e76b2aa8582a7307565df3e12c30d8add29c14
jorge aguilera <jagedn@gmail.com>
2023-04-16 14:15:05 +0200

    06 user-details

--- micronaut-security-jwt-gradle-java/src/main/java/example/micronaut/HomeController.java
+++ micronaut-security-jwt-gradle-java/src/main/java/example/micronaut/HomeController.java
@@ -1,5 +1,6 @@
 package example.micronaut;
 
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -8,6 +9,8 @@ import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 
 import java.security.Principal;
+import java.util.Map;
+import java.util.Random;
 
 @Secured(SecurityRule.IS_AUTHENTICATED) // <1>
 @Controller  // <2>
@@ -18,4 +21,21 @@ public class HomeController {
     public String index(Principal principal) {  // <4>
         return principal.getName();
     }
+
+    @Produces(MediaType.TEXT_PLAIN)
+    @Get("/user-details") // <3>
+    public HttpResponse<String> userDetails(Principal principal) {  // <4>
+        return
+                HttpResponse
+                        .ok(principal.getName())
+                        .headers(
+                                Map.of(
+                                        "x-user-id", new Random().nextInt()+"",
+                                        "x-user-name", principal.getName(),
+                                        "x-random", new Random().nextInt()+""
+                                )
+                        );
+    }
+
+
 }
