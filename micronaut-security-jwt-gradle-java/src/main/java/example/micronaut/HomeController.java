25da79bc25adffcc90ca9f29546ea7ca4b51ec8a
Jorge Aguilera <jorgea@getbaraka.com>
2023-05-21 12:21:30 +0200

    add timeout test

--- micronaut-security-jwt-gradle-java/src/main/java/example/micronaut/HomeController.java
+++ micronaut-security-jwt-gradle-java/src/main/java/example/micronaut/HomeController.java
@@ -5,12 +5,14 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 
 import java.security.Principal;
 import java.util.Map;
 import java.util.Random;
+import java.util.Optional;
 
 @Secured(SecurityRule.IS_AUTHENTICATED) // <1>
 @Controller  // <2>
@@ -18,8 +20,16 @@ public class HomeController {
 
     @Produces(MediaType.TEXT_PLAIN)
     @Get // <3>
-    public String index(Principal principal) {  // <4>
+    public String index(Principal principal, @QueryValue("sleep")Optional<Integer> sleep) {  // <4>
+        if( sleep.isPresent()){
+            try{
+                Thread.sleep(sleep.get()*1000);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
         return principal.getName();
+
     }
 
     @Produces(MediaType.TEXT_PLAIN)
